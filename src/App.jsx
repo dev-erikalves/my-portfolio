a495c42bd7e37d9c1c7b9b9d5edf2a331d8f6460
Erik Alves <dev.eriksantos@gmail.com>
2023-05-24 18:58:43 -0300

    project started, work environment ready

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import MainContent from "./components/MainContent/MainContent.jsx";
+import SideBar from "./components/SideBar/SideBar.jsx";
+
+import "./styles/app.scss";
+
+function App() {
+
+  return (
+    <>
+        <h1>Erik Alves</h1>
+        <MainContent />
+        <SideBar />
+    </>
+  )
+}
+
+export default App
