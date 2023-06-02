690c925561ced8d3ff2c1716bd87451c85b391a7
Erik Alves <dev.eriksantos@gmail.com>
2023-06-01 21:31:05 -0300

    Relocation of media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Name from "./components/Name/Name.jsx"
 import SideBar from "./components/SideBar/SideBar.jsx";
 import MainContent from "./components/MainContent/MainContent.jsx";
-import "./styles/app.scss";
 import Footer from "./components/Footer/Footer.jsx";
+import "./styles/app.scss";
 
 export default function App() {
   return (
