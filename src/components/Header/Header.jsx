56acae5feb76c7c419de2abf511076bf2c65b400
Erik Alves <dev.eriksantos@gmail.com>
2023-06-06 18:32:30 -0300

    feat(components): include header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import Name from '../Name/Name.jsx'
+import styles from './styles.module.scss'
+
+export default function() {
+    return(
+        <header className={styles.header}>
+            <Name />
+            <nav>
+                <ul>
+                    <li><a href="#sobreMim">Sobre Mim</a></li>
+                    <li><a href="#habilidades">Habilidades</a></li>
+                    <li><a href="#projetos">Projetos</a></li>
+                    <li><a href="#contatos">Contatos</a></li>
+                </ul>
+            </nav>
+        </header>    
+    )
+}
