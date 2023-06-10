b642aa0ed3b1e9c884108ff6798d60e1f3853a8d
Erik Alves <dev.eriksantos@gmail.com>
2023-06-10 15:22:06 -0300

    refactor(header): Style and code improvements

--- src/components/Header/components/Name/Name.jsx
+++ src/components/Header/components/Name/Name.jsx
@@ -1,20 +1,21 @@
 import styles from "./styles.module.scss";
-import { useTypewriter } from 'react-simple-typewriter'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 export default function Name() {
     const [text] = useTypewriter({
-        words: ['<Erik Alves>'],
+        words: ['Bem vindo!', 'Eu sou...', '<Erik Alves>'],
         loop: {},
         typeSpeed: 190,
         deleteSpeed: 90,
     })
     return (
-        <div>
-            <span className={styles.nameContainer}></span>
-            <h1 id="header">
+        <>
+            <h1 style={{display: 'flex'}}>
                 <span className={styles.name}>{text}</span>
+                <span style={{color: 'white', margin: '0 0 10px'}}>
+                    <Cursor cursorStyle='|'/>
+                </span>
             </h1>
-        </div>
+        </>
     )
 }
-// id="toTop" for btn toTop in footer
