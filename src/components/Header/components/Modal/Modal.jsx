7a6ee5f4126b37545f97e1a83f4bbe95ffa6c839
Erik Alves <dev.eriksantos@gmail.com>
2023-06-11 20:55:57 -0300

    refactor(header/modal): Removal of unnecessary things and minor style changes

--- src/components/Header/components/Modal/Modal.jsx
+++ src/components/Header/components/Modal/Modal.jsx
@@ -1,22 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/js/dist/modal'
 import styles from "./styles.module.scss"
 
-export default function Modal(){
+export default function Modal() {
     return (
         <>
-            <div className="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+            <div className="modal" id="exampleModalToggle">
                 <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content" id={styles.modalContent}>
+                    <div className="modal-content bg-dark">
                         <div className="modal-header">
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <i type="button" className="btn-close" data-bs-dismiss="modal"></i>
                         </div>
-                        <div className="modal-body" id={styles.modalBody}>
-                                <a href="https://wa.me/+558994307093" target='_blank'>Whatsapp</a>
-                                <a href="mailto:dev.eriksantos@gmail.com"  target='_blank'>Email</a>
-                                <a href="https://github.com/dev-erikalves"  target='_blank'>Github</a>
-                                <a href="https://www.linkedin.com/in/dev-erikalves/"  target='_blank'>Linkedin</a>
-                                <a href="https://www.instagram.com/developerik.web/"  target='_blank'>Instagram</a>
+                        <div id={styles.modalBody} className='p-4 gap-4 d-flex'>
+                            <a href="https://wa.me/+558994307093">Whatsapp</a>
+                            <a href="mailto:dev.eriksantos@gmail.com" target='_blank'>Email</a>
+                            <a href="https://github.com/dev-erikalves" target='_blank'>Github</a>
+                            <a href="https://www.linkedin.com/in/dev-erikalves/" target='_blank'>Linkedin</a>
+                            <a href="https://www.instagram.com/developerik.web/" target='_blank'>Instagram</a>
                         </div>
                     </div>
                 </div>
