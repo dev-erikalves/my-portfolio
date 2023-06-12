00f6d194206dd714a680069dd1f969d317616ef9
Erik Alves <dev.eriksantos@gmail.com>
2023-06-11 22:05:34 -0300

    refactor(header): Style changes, added other media queries and deleted unnecessary stuff

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import Name from './components/Name/Name.jsx';
 import styles from './styles.module.scss';
-import Modal  from './components/Modal/Modal.jsx'
+import Modal from './components/Modal/Modal.jsx'
 
 export default function Header() {
     return (
         <header id='header'>
             <Name />
             <nav>
-                <LinkNav href="#contentAbout" name="Sobre"/>
-                <LinkNav href="#contentSkills" name="Habilidades"/>
-                <LinkNav href="#contentProjects" name="Projetos"/>
+                <LinkNav href="#contentAbout" name="Sobre" />
+                <LinkNav href="#contentSkills" name="Habilidades" />
+                <LinkNav href="#contentProjects" name="Projetos" />
                 <button className={styles.linksNav} data-bs-target="#exampleModalToggle"
-                data-bs-toggle="modal">Redes</button>
+                    data-bs-toggle="modal">Redes</button>
                 <Modal />
             </nav>
         </header>
@@ -22,6 +22,6 @@ export const LinkNav = ({ href, name }) => {
     return (
         <a href={href}>
             <button className={styles.linksNav}>{name}</button>
-        </a> 
+        </a>
     )
 }
