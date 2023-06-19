60176b76c8a860914f6f135bb9686dfffd6d9ea2
Erik Alves <dev.eriksantos@gmail.com>
2023-06-19 20:07:43 -0300

    deleted mainContent folder, its components migrated to src/pages/home

--- src/components/Card/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss"
-
-export default function Card({ className, children }){
-    return(
-        <section className={`${styles.card} ${className}`}>
-            {children}
-        </section>
-    )
-}
