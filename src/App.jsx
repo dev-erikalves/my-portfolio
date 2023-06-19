2b26c5c1399bd6593076a4b160c8d315bbf548d1
Erik Alves <dev.eriksantos@gmail.com>
2023-06-19 20:03:31 -0300

    refactor(App.jsx): Imported new components and included for new layout, including classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import Header from "./pages/home/Header/Header.jsx"
 import SideBar from "./pages/home/SideBar/SideBar.jsx";
-import MainContent from "./pages/home/MainContent/MainContent.jsx";
+import AboutMe from "./pages/home/ContentAboutMe/AboutMe.jsx"
+import Skills from "./pages/home/ContentSkills/Skills.jsx"
+import Projects from "./pages/home/ContentProjects/Projects.jsx"
 import Footer from "./pages/home/Footer/Footer.jsx";
 import "./styles/app.scss";
 
 export default function App() {
   return (
     <>
-        <Header />
-        <div className="mainAndSidebar">
+      <Header />
+
+      <main className="mainContent">
+
+        <div className="sidebarAboutAndSkills">
           <SideBar />
-          <MainContent />
+
+          <div className="aboutAndSkills">
+            <AboutMe />
+            <Skills />
+          </div>
         </div>
-          <Footer />
+        
+        <Projects />
+      </main>
+
+      <Footer />
     </>
   )
 }
