76049a67c48578ae926cf220e2991037d1a9338a
Erik Alves <dev.eriksantos@gmail.com>
2023-06-20 18:05:46 -0300

    refactor(Projects.jsx): Description change

--- src/pages/home/ContentProjects/Projects.jsx
+++ src/pages/home/ContentProjects/Projects.jsx
@@ -7,7 +7,7 @@ export default function ContentProjects(){
         <section id="contentProjects" className={styles.contentProjects}>
             <TitleAndDescription
                 title="Projetos"
-                description="Veja um pouco do que andei aprontando nos últimos dias. Todos os meus repositórios com todos os projetos, para ficar a um passo de saber mais sobre cada um, dê um pulinho na README.md! Te vejo por ai &#x1F918;&#x1F525;."
+                description="Veja um pouco do que andei aprontando nos últimos dias. Alguns dos meus principais projetos. Para ficar a um passo de saber mais sobre cada um, clique no icone 'i' nos cards! Te vejo por ai &#x1F918;&#x1F525;."
             />
             <TitlebarImageList/>
             <a className={styles.projectsBtn} href="https://github.com/dev-erikalves?tab=repositories" target="_blank">Outros</a>
