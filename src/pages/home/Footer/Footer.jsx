f10711fef104d672a2e8f19a7ebbb6452a600085
dev-erikalves <dev.eriksantos@gmail.com>
2023-07-13 11:47:47 -0300

    feat(Footer.jsx): Added a new way the btnToTop button takes you to the top of the page

--- src/pages/home/Footer/Footer.jsx
+++ src/pages/home/Footer/Footer.jsx
@@ -2,16 +2,22 @@ import iconUpToTop from "@assets/arrow-up-circle.svg"
 import styles from "./style.module.scss";
 
 export default function Footer() {
+    const ScrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    };
     return (
-        <>
-            <footer>
-                <p>Feito com carinho, além de muito &#x2764;</p>
-                <p className={styles.copy}>&copy; 2023 - All rights reserveds</p>
+        <footer>
+            <p>Feito com carinho, além de muito &#x2764;</p>
+            <p className={styles.copy}>&copy; 2023 - All rights reserveds</p>
 
-                <a href="#header" className={styles.toTopBtn}>
-                    <img src={iconUpToTop} alt="Icon de flecha apontando para cima" />
-                </a>
-            </footer>
-        </>
-    )
-}
+            <button
+                className={styles.btnToTop}
+                onClick={ScrollToTop}>
+                <img src={iconUpToTop} alt="Icone de flecha apontando para cima" />
+            </button>
+        </footer>
+    );
+};
