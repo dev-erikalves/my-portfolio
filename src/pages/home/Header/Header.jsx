53316ad7139223efee06f314f1e0b34a98bf538c
dev-erikalves <dev.eriksantos@gmail.com>
2023-07-12 23:24:16 -0300

    feat(Header.jsx): Header is now hidden when scrolling down and displayed when scrolling up

--- src/pages/home/Header/Header.jsx
+++ src/pages/home/Header/Header.jsx
@@ -1,27 +1,59 @@
+import React, { useState, useEffect } from 'react';
 import Name from './components/Name/Name.jsx';
 import styles from './styles.module.scss';
-import Modal from './components/Modal/Modal.jsx'
+import Modal from './components/Modal/Modal.jsx';
 
-export default function Header() {
-    return (
-        <header id='header'>
-            <Name />
-            <nav>
-                <LinkNav href="#contentAbout" name="Sobre" />
-                <LinkNav href="#contentSkills" name="Habilidades" />
-                <LinkNav href="#contentProjects" name="Projetos" />
-                <button className={styles.linksNav} data-bs-target="#exampleModalToggle"
-                    data-bs-toggle="modal">Redes</button>
-                <Modal />
-            </nav>
-        </header>
-    )
-}
+const Header = () => {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-export const LinkNav = ({ href, name }) => {
-    return (
-        <a href={href}>
-            <button className={styles.linksNav}>{name}</button>
-        </a>
-    )
-}
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) {
+        setShow(false);
+      } else {
+        setShow(true);
+      }
+
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      return () => {
+        window.removeEventListener('scroll', controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
+  return (
+    <header id='header' className={show ? styles.headerVisible : styles.headerHidden}>
+      <Name />
+      <nav>
+        <LinkNav href="#contentAbout" name="Sobre"/>
+        <LinkNav href="#contentSkills" name="Habilidades"/>
+        <LinkNav href="#contentProjects" name="Projetos"/>
+        <button
+          className={styles.linksNav}
+          data-bs-target="#exampleModalToggle"
+          data-bs-toggle="modal"
+        >
+          Redes
+        </button>
+        <Modal />
+      </nav>
+    </header>
+  );
+};
+export const LinkNav = ({ href, name}) => {
+  return (
+    <a href={href}>
+      <button className={styles.linksNav}>{name}</button>
+    </a>
+  );
+};
+
+export default Header;
