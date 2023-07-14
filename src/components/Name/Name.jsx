ab3b713ee657d8a94cdfeff091400300d421eee3
dev-erikalves <dev.eriksantos@gmail.com>
2023-07-14 13:53:18 -0300

    refactor(Name.jsx): Adjusted animation settings, removed component inline styling,

--- src/components/Name/Name.jsx
+++ src/components/Name/Name.jsx
@@ -5,17 +5,13 @@ export default function Name() {
     const [text] = useTypewriter({
         words: ['Bem vindo!', 'Eu sou...', '<Erik Alves>'],
         loop: {},
-        typeSpeed: 190,
+        typeSpeed: 290,
         deleteSpeed: 90,
     })
     return (
-        <>
-            <h1 style={{display: 'flex'}}>
-                <span className={styles.name}>{text}</span>
-                <span style={{color: 'white', margin: '0 0 10px'}}>
-                    <Cursor cursorStyle='|'/>
-                </span>
-            </h1>
-        </>
+        <div className={styles.nameContainer}>
+            <h1>{text}</h1>
+            <Cursor cursorColor="white" cursorStyle='|' />
+        </div>
     )
 }
